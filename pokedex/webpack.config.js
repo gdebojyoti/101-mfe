ea067c64423d3bc0256129bdb179ef779c5b7c56
Debojyoti Ghosh <gdebojyoti@users.noreply.github.com>
2026-06-15 21:35:15 +0530

    CORS headers added in pokedex app.

--- pokedex/webpack.config.js
+++ pokedex/webpack.config.js
@@ -3,7 +3,10 @@ const HtmlWebpackPlugin = require("html-webpack-plugin")
 module.exports = {
   mode: "development",
   devServer: {
-    port: 4001
+    port: 4001,
+    headers: {
+      "Cross-Origin-Resource-Policy": "cross-origin"
+    }
   },
   plugins: [
     new HtmlWebpackPlugin({
