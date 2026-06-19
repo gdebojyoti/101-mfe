f83dafe506d2d5e5a4bc2dd5efd8a1401dd288e1
Debojyoti Ghosh <gdebojyoti@users.noreply.github.com>
2026-06-19 17:37:00 +0530

    pokedex & quiz apps commented out temporarily.

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,9 +1,9 @@
-import mount from "pokedex/index";
-import { mount as quizMount } from "quiz/index";
+// import mount from "pokedex/index";
+// import { mount as quizMount } from "quiz/index";
 import { mount as profileMount } from "profile/index";
 
 console.log("I got in @ Container!");
 
-mount(document.getElementById("pokedex-app"));
-quizMount(document.getElementById("quiz-app"));
+// mount(document.getElementById("pokedex-app"));
+// quizMount(document.getElementById("quiz-app"));
 profileMount(document.getElementById("profile-app"));
